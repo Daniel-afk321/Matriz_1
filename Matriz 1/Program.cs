549740008b503596bdc0304cd371802abf9e2d94
Daniel-afk321 <83432335+Daniel-afk321@users.noreply.github.com>
2021-10-06 21:38:36 -0300

    Adicionar arquivos de projeto.

---
+++ Matriz 1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Matriz_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // entra dos dados 
+            int n = int.Parse(Console.ReadLine()); 
+            
+            int[,] mat = new int[n, n]; 
+            
+            for (int i = 0; i < n; i++) {
+
+                // esta criando uma matriz de subcadeias
+                string[] values = Console.ReadLine().Split(' '); 
+                
+                
+                for (int j = 0; j < n; j++) { 
+                    
+                    mat[i, j] = int.Parse(values[j]); 
+                } 
+            }
+            // esta mostrando a diagonal principal 
+            Console.WriteLine("Diagonal principal:");
+            for (int i = 0; i < n; i++) {
+                
+                Console.Write(mat[i,i] + " ");
+            
+            }
+            Console.WriteLine();
+            // esta fazendo para mostrar os numeros negativos da matriz
+            int count = 0;
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    if (mat[i,j] < 0) {count++;
+
+                    }
+                }
+            }
+            
+            // resultado do numero negativo
+            Console.WriteLine("Numero negativo: " + count);
+        }
+    }
+}
